2d312192207e7f32ffb6dab5343168ba6015fbd0
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-02-06 19:48:20 +0200

    Added onOpen.js file. Main menu with ui for setup API key

---
+++ onOpen.js
@@ -0,0 +1,30 @@
+/**
+ * Simple trigger. When user open document,
+ * add item to main menu.
+ */
+function onOpen() {
+  const ui = SpreadsheetApp.getUi();
+
+  ui.createMenu('Setup')
+    .addItem('Update DeepL API key', 'updateApiKeyDialog')
+    .addToUi();
+}
+
+/**
+ * Show prompt to update DeepL API key.
+ * @see https://www.deepl.com/docs-api.html?part=accessing
+ * Then save it to script properties.
+ * To find where it stored, in current  editor window, visit:
+ * File -> Project properties -> Script properties tab
+ */
+function updateApiKeyDialog() {
+  const ui = SpreadsheetApp.getUi();
+  const title = 'Update DeepL API key';
+  const prompt = 'Warning: previous key will be overwritten.';
+  const buttons = ui.ButtonSet.OK_CANCEL;
+  const response = ui.prompt(title, prompt, buttons);
+  if (response.getSelectedButton() === ui.Button.OK) {
+    const apiKey = response.getResponseText().trim();
+    PropertiesService.getScriptProperties().setProperty('DEEPL_API_KEY', apiKey);
+  }
+}
